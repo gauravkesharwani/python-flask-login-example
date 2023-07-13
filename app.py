437626039cb2975199a560a4dd40624ae0d9ddfb
Gaurav <gauravkesharwani@gmail.com>
2023-07-13 10:51:23 -0700

    first comit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def create_app():
 
     db.create_all()
 
-    app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
+    app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuop'
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
